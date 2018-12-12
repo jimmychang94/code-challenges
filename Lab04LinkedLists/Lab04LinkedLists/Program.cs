14cce2f44f160666fe586c32473369caf78c2765
Jimmy Chang <jimmyfchang94@gmail.com>
2018-12-12 00:04:40 -0800

    Creates linked list and node classes, also creates several methods for linked lists.

---
+++ Lab04LinkedLists/Lab04LinkedLists/Program.cs
@@ -0,0 +1,23 @@
+﻿using Lab04LinkedLists.Classes;
+using System;
+
+namespace Lab04LinkedLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            list.Partition();
+            //list.Reverse();
+            list.Traverse(Console.WriteLine);
+            //Console.WriteLine(list.KthLast(3));
+            //Console.WriteLine(list.FindTheMiddle(list.Head, list.Head));
+        }
+    }
+}
