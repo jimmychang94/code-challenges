ff8d213677ae276d709724944d4b4cddfafbe3d9
Jimmy Chang <jimmyfchang94@gmail.com>
2019-01-10 11:26:22 -0800

    Completes the code challenge for trees

---
+++ Lab07Trees/Lab07Trees/Classes/Node.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab07Trees.Classes
+{
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+
+        public Node(int value)
+        {
+            Value = value;
+        }
+
+        public void Add(int value)
+        {
+            if(value < Value)
+            {
+                if (Left != null) Left.Add(value);
+                else Left = new Node(value);
+            }
+            else
+            {
+                if (Right != null) Right.Add(value);
+                else Right = new Node(value);
+            }
+        }
+
+        public void Find(int value, Action<int> callback)
+        {
+            if (value == Value)
+            {
+                callback(Value);
+            }
+            if (value < Value)
+            {
+                if (Left != null) Left.Find(value, callback);
+            }
+            else
+            {
+                if (Right != null) Right.Find(value, callback);
+            }
+        }
+
+        public void PostOrder(Action<int> callback)
+        {
+            if (Left != null) Left.PostOrder(callback);
+            if (Right != null) Right.PostOrder(callback);
+            callback(Value);
+        }
+
+        public void PreOrder(Action<int> callback)
+        {
+            callback(Value);
+            if (Left != null) Left.PreOrder(callback);
+            if (Right != null) Right.PreOrder(callback);
+        }
+
+        public void InOrder(Action<int> callback)
+        {
+            if (Left != null) Left.InOrder(callback);
+            callback(Value);
+            if (Right != null) Right.InOrder(callback);
+        }
+
+        public void CreateBST(int[] array, int left, int right)
+        {
+            if (left >= right)
+            {
+                return;
+            }
+            int mid = (left + right) / 2;
+            if (array[mid] < Value)
+            {
+                Left = new Node(array[mid]);
+                Left.CreateBST(array, left, mid);
+                Left.CreateBST(array, mid + 1, right);
+            }
+            if (array[mid] > Value)
+            {
+                Right = new Node(array[mid]);
+                Right.CreateBST(array, left, mid);
+                Right.CreateBST(array, mid + 1, right);
+            }
+        }
+    }
+}
---
+++ Lab07Trees/Lab07Trees/Program.cs
@@ -0,0 +1,54 @@
+﻿using Lab07Trees.Classes;
+using System;
+using System.Collections.Generic;
+
+namespace Lab07Trees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TestBSTVerification();
+            TestCreateBST();
+        }
+
+        static void TestCreateBST()
+        {
+            int[] testArray = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+            Node root = BinaryTree.CreateBST(testArray);
+            BinaryTree bt = new BinaryTree();
+            bt.Root = root;
+            bt.BreadthFirst(Console.WriteLine);
+        }
+
+        static void TestBSTVerification()
+        {
+            Node root = new Node(10);
+            root.Left = new Node(5);
+            root.Right = new Node(15);
+            root.Left.Left = new Node(1);
+            root.Left.Right = new Node(7);
+            root.Right.Left = new Node(12);
+            root.Right.Right = new Node(21);
+
+            bool test = BSTVerification(root);
+
+            Console.WriteLine(test);
+        }
+
+        static bool BSTVerification(Node root)
+        {
+            List<int> list = new List<int>();
+            root.InOrder(list.Add);
+            bool value = true;
+            for(int i = 0; i < list.Count - 1; i ++)
+            {
+                if (list[i] > list[i + 1])
+                {
+                    value = false;
+                }
+            }
+            return value;
+        }
+    }
+}
